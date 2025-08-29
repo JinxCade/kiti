32207e35ce95433230ef6b56e6d1fc6f1ab689c6
JinxCade <76182826+JinxCade@users.noreply.github.com>
2025-08-29 00:04:07 -0500

    Update temp.bat

--- temp.bat
+++ temp.bat
@@ -4,7 +4,23 @@ title DOWNLOAD
 cls 
 echo Downloader v1.0.0
 echo Begin
-start powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/kiti/resources/app/ping.txt','ping.txt')
-start powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/kiti/resources/app/server_data.json','server_data.json')
+start powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/kiti/ping.txt','resources/app/ping.txt')
+start powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/kiti/server_data.json','resources/app/server_data.json')
+start powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/kiti/minimum_server_version.txt','resources/app/minimum_server_version.txt')
+start powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/kiti/minimum_client_version.txt','resources/app/minimum_client_version.txt')
+start powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/kiti/recommended_client_version.txt','resources/app/recommended_client_version.txt')
+start powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/kiti/recommended_server_version.txt','resources/app/recommended_server_version.txt')
+start powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/kiti/format_version.txt','resources/app/format_version.txt')
+start powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/kiti/lang/en.json','resources/app/lang/en.json')
+start powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/kiti/lang/jp.json','resources/app/lang/jp.json')
+start powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/kiti/shop/shop.json','resources/app/shop/shop.json')
+start powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/kiti/shop/test_shop.json','resources/app/shop/test_shop.json')
+start powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/kiti/shop/pjsk_shop.json','resources/app/shop/pjsk/shop.json')
+start powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/kiti/shop/lang/en.json','resources/app/shop/lang/en.json')
+start powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/kiti/shop/lang/jp.json','resources/app/shop/lang/jp.json')
+start powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/kiti/gm_mus/pjsk/Engeki_3D.mp4','resources/app/gm_mus/pjsk/Engeki_3D.mp4')
+start powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/kiti/gm_mus/pjsk/Saisei_3D.mp4','resources/app/gm_mus/pjsk/Saisei_3D.mp4')
+start powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/kiti/gm_mus/pjsk/IDSMILE_2D.mp4','resources/app/gm_mus/pjsk/IDSMILE_2D.mp4')
+start powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/kiti/gm_mus/pjsk/Kitty_2D.mp4','resources/app/gm_mus/pjsk/Kitty_2D.mp4')
 pause 
 exit
